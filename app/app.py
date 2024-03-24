0b682236a0691cf8d9a18ee861c5bdb087e5280c
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-03-24 10:21:33 -0300

    fix: name utf-8

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify,json
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 # Carregar os dados do arquivo JSON
-with open('data.json', 'r') as f:
+with open('data.json', 'r',encoding='utf8') as f:
     data = json.load(f)
 
 #all  trips
